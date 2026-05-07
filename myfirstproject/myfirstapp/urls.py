2bba0a102db5a0420c095f1e52bda4962afa3fc6
yildiz68 <ydzali07@gmail.com>
2026-05-07 11:40:19 +0200

    ed

--- myfirstproject/myfirstapp/urls.py
+++ myfirstproject/myfirstapp/urls.py
@@ -3,4 +3,5 @@ from. import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('formulaire/', views.formulaire),
 ]
--- myfirstproject/myfirstapp/views.py
+++ myfirstproject/myfirstapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'myfirstapp/index.html')
+
+def formulaire(request):
+    return render(request,'myfirstapp/formulaire.html')
